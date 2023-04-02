44f22ed4b7781cd811225532aa8fa58d1865ca54
Marco Tulio <marcotfrancosilva@gmail.com>
2023-04-01 21:19:07 -0300

    Starting the project

---
+++ src/main/java/com/api/videosharingplatform/VideosharingplatformApplication.java
@@ -0,0 +1,13 @@
+package com.api.videosharingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideosharingplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideosharingplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/videosharingplatform/VideosharingplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.videosharingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideosharingplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
